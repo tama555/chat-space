a69702c05cd4ff81322887b6b8b0790942c10caf
tama555 <yoshi4548797@yahoo.co.jp>
2020-06-18 21:23:49 +0900

    initial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
+
+# ailsでは、悪意のあるユーザーの入力に対してセキュリティ対策を行わないと保存できない仕組みがあります。
+# 通常のテーブルに保存する際はストロングパラメータを使用しましたが、deviseを使ったモデルの場合は方法が異なります。
+# 上のコードは、deviseの公式サイトで紹介されている書き方です。そのまま使用するものという理解で問題ない
