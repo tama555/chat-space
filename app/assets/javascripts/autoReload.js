b3fe6e239e0e4608b2324ac0eaaa7720d4554bbd
tama555 <yoshi4548797@yahoo.co.jp>
2020-07-14 17:09:38 +0900

    reroad

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -42,7 +42,7 @@ $(function(){
 
   let reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    let last_message_id = $('.message:last').data("message-id");
+    let last_message_id = $('.message:last').data("message-id") || 0;
     $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
